a8e4dca7b977a327cc3806984284c40d01d0aba9
Ahcmad25 <ahcmadangagao@gmail.com>
2025-11-01 20:33:59 +0800

    Modified DB config

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize(
   process.env.DB_NAME ?? "providers_nearby",
   process.env.DB_USER ?? "root",
-  process.env.DB_PASS ?? "Javapro_0948",
+  process.env.DB_PASS ?? "",
   {
     host: process.env.DB_HOST ?? "127.0.0.1",
     port: Number(process.env.DB_PORT ?? 3306),
